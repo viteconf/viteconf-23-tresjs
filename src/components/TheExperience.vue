ed965f5bd174da693b10a2463e8277cb00220f10
patak <matias.capeletto@gmail.com>
2023-09-21 16:41:45 +0200

    initial commit

---
+++ src/components/TheExperience.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { extend } from '@tresjs/core';
+
+import { reactive } from 'vue';
+import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three';
+import { TresCanvas } from '@tresjs/core';
+import { RoundedBoxGeometry } from 'three-stdlib';
+import { OrbitControls, Levioso } from '@tresjs/cientos';
+
+const gl = {
+  clearColor: '#82DBC5',
+  shadows: true,
+  alpha: false,
+  shadowMapType: BasicShadowMap,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: NoToneMapping,
+};
+
+extend({ RoundedBoxGeometry });
+</script>
+
+<template>
+  <TresCanvas v-bind="gl">
+    <TresPerspectiveCamera :position="[9, 9, 9]" />
+    <OrbitControls />
+    <TresMesh cast-shadow>
+      <TresRoundedBoxGeometry :args="[1, 1, 1, 7, 0.2]" />
+      <TresMeshToonMaterial color="#82DBC5" />
+    </TresMesh>
+    <TresAxesHelper :args="[1]" />
+    <TresDirectionalLight :position="[0, 2, 4]" :intensity="1.2" cast-shadow />
+  </TresCanvas>
+</template>
